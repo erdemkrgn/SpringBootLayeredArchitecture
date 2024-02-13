499cd234b1a6462239f78e4fa111d9f4a12df2a4
erdemkrgn <muammererdemkaragun@hotmail.com>
2024-02-13 17:40:18 +0300

    Initial commit

---
+++ src/main/java/com/erdemkrgn/springbootstudies/SpringbootstudiesApplication.java
@@ -0,0 +1,13 @@
+package com.erdemkrgn.springbootstudies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootstudiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootstudiesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/erdemkrgn/springbootstudies/SpringbootstudiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erdemkrgn.springbootstudies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootstudiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
